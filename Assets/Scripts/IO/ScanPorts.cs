1802ecca209f15ccd81ebe3d0f96518b799dd513
Gerndal <gerndal@msn.com>
2021-04-02 14:27:58 +0900

    소스 정리.

--- Assets/Scripts/IO/ScanPorts.cs
+++ Assets/Scripts/IO/ScanPorts.cs
@@ -8,81 +8,148 @@ using System.IO.Ports;
 
 public class ScanPorts: MonoBehaviour
 {
-    public string USB_PortName;
-    public int baudrate = 76800;
+    private SerialPort stream;
+    public  int baudrate            = 57600;
+    public  int readTimeout         = 50;
+    public  float handShakeDuration = 1.0f;
+
+    private string strEnd         = "\n";
+    private const float Frequency = 0.06f;
 
     [Space]
-    public  ArduinoConnect arduinoConnect;
     public  TalkToArduino talkToArduino;
-
-    private List<string> usbPortNames;
-    private bool NoPortYet = true;
+    private bool isHandShake = false;
 
     void Start()
     {
-        StartCoroutine(ScanForArduinoPort());
+        StartCoroutine(CoScanForArduinoPort());
     }
 
-    public void StopScan()
+    public void SerialOpen(string _portName, int _baudrate)
     {
-        StopAllCoroutines();
+        Debug.Log("<color=green>" + "Trying with port: " + _portName + "</color>\n");
+        
+        stream             = new SerialPort(_portName, _baudrate, Parity.None, 8, StopBits.One);
+        stream.ReadTimeout = readTimeout;
+        stream.Open();
     }
 
-    public IEnumerator ScanForArduinoPort()
+    public void SerialWrite(string message)
+    {
+        try
+        {
+            stream.WriteLine(message);
+            stream.BaseStream.Flush();
+        }
+        catch (TimeoutException)
+        {
+
+        }
+    }
+
+    public string SerialRead(int timeout=0)
+    {
+        stream.ReadTimeout = timeout;
+        try
+        {
+            return stream.ReadLine();
+        }
+        catch (TimeoutException)
+        {
+            return null;
+        }
+    }
+
+    public IEnumerator CoAsynchronousSerialRead(Action<string> callback, float timeout=float.PositiveInfinity)
+    {
+        DateTime initialTime = DateTime.Now;
+        TimeSpan diff        = default(TimeSpan);
+
+        do
+        {
+            var dataString = SerialRead(readTimeout);
+            if (dataString != null)
+            {
+                callback(dataString);
+                yield break;
+            }
+
+            yield return new WaitForEndOfFrame();
+
+            diff = DateTime.Now - initialTime;
+        } while (diff.Milliseconds < timeout);
+    }
+
+    public IEnumerator CoScanForArduinoPort()
     {
         Debug.Log("<color=green>" + "Scanning for arduino port" + "</color>\n");
 
-        NoPortYet = true;
-        while (NoPortYet)
+        while (isHandShake == false)
         {
-            yield return StartCoroutine(GetArduinoPort());
-        }
-    }
-
-    public IEnumerator GetArduinoPort()
-    {
-        yield return StartCoroutine(ScanUSBPortNames());
-
-        foreach (var port in usbPortNames)
-        {
-            arduinoConnect.Open(port, baudrate);
-            yield return StartCoroutine(talkToArduino.SendHandshake());
-            Debug.Log("<color=green>" + "Trying with port: " + port + "</color>\n");
-
-            if (talkToArduino.GotHandShake)
+            foreach (var port in ScanUSBPortNames())
             {
-                USB_PortName = port;
-                NoPortYet    = false;
+                SerialOpen(port, baudrate);
+                yield return StartCoroutine(CoSendHandshake());
 
-                Debug.Log("<color=green>" + "Arduino port is: " + USB_PortName + "</color>\n");
-                talkToArduino.StartTalkingToArduino();
-                yield break;
+                if (isHandShake) yield break;
+                yield return new WaitForSeconds(handShakeDuration);
             }
-            yield return new WaitForSeconds(0.5f);
-        }
-    }
-
-    //list usb ports on mac
-    IEnumerator ScanUSBPortNames()
-    {
-            usbPortNames = new List<string>();
-        var p            = System.Environment.OSVersion.Platform;
-
-        // Are we on Unix?
-        if ((p == PlatformID.Unix  ) ||
-            (p == PlatformID.MacOSX))
-        {
-            string[] ttys = System.IO.Directory.GetFiles("/dev/", "tty.*");
             yield return new WaitForEndOfFrame();
+        }
+    }
 
-            foreach (string dev in ttys)
-            {
-                if (dev.StartsWith("/dev/tty.")) usbPortNames.Add(dev);
-            }
+    IEnumerator CoSendSensorIndexToArduino()
+    {
+        while (true)
+            yield return CoAsynchronousSerialRead(s => talkToArduino.CheckCallback(s), Frequency);
+    }
+
+    public IEnumerator CoSendHandshake()
+    {
+        Debug.Log("<color=green>" + "handshake sent... waiting for reply" + "</color>\n");
+
+        SerialWrite("GameIsland" + strEnd);
+        yield return CoAsynchronousSerialRead(s => CheckHandshake(s), handShakeDuration);
+    }
+
+    void CheckHandshake(string str)
+    {
+        Debug.Log(str);
+
+        isHandShake = (str == "GameIsland");
+        if (isHandShake == false) return;
+
+        talkToArduino.CheckCallback(str);
+
+        Debug.Log("<color=green>" + "Arduino Connected Port is : " + stream.PortName + "</color>\n");
+        StartCoroutine(CoSendSensorIndexToArduino());
+    }
+
+    private List<string> ScanUSBPortNames()
+    {
+        var usbPortNames = new List<string>();
+        var platform     = System.Environment.OSVersion.Platform;
+
+        if ((platform == PlatformID.Unix  ) ||
+            (platform == PlatformID.MacOSX))
+        {
+            var ttys = System.IO.Directory.GetFiles("/dev/", "tty.*");
+            ttys?.ToList().ForEach(dev => 
+            { 
+                if (dev.StartsWith("/dev/tty.")) usbPortNames.Add(dev); 
+            });
         }
         else
         {
-            SerialPort.GetPortNames().ToList().ForEach(p => usbPortNames.Add(p));
+            SerialPort.GetPortNames()?.ToList().ForEach(p => usbPortNames.Add(p));
         }
+
+        return usbPortNames;
+    }
+
+    void OnApplicationQuit()
+    {
+        Debug.Log("OnApplicationQuit");
+        stream.Close();
     }
 }
--- Assets/Scripts/IO/TalkToArduino.cs
+++ Assets/Scripts/IO/TalkToArduino.cs
@@ -1,71 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TalkToArduino : MonoBehaviour
+public class TalkToArduino: MonoBehaviour
 {
-    //connect class
-    public ArduinoConnect arduinoConnect;
-
     //call sensor readings on Arduino
-    public string[] sensorChars = { "A", "B", "C" };
-    private string strEnd = "\n";
-    private int sensorIndex = 0;
+    public  string[] sensorChars  = { "A", "B", "C" };
 
-    const float Frequency = 0.06f;
-
-    [HideInInspector]
-    public bool GotHandShake = false;
-
-    public void StartTalkingToArduino()
-    {
-        Camera.main.GetComponent<Camera>().backgroundColor = Color.green;
-        InvokeRepeating("SendSensorIndexToArduino", Frequency, Frequency);
-    }
-
-    public void SendSensorIndexToArduino()
-    {
-        arduinoConnect.WriteToArduino(sensorChars[(sensorIndex++) % sensorChars.Length] + strEnd);
-        StartCoroutine(arduinoConnect.AsynchronousReadFromArduino(s => CheckCallback(s), Frequency));
-    }
-
-    public IEnumerator SendHandshake()
-    {
-        Debug.Log("<color=green>" + "handshake sent... waiting for reply" + "</color>\n");
-        arduinoConnect.WriteToArduino("GameIsland" + strEnd);
-
-        yield return arduinoConnect.AsynchronousReadFromArduino(s => CheckHandshake(s), 1);
-    }
-
-    void CheckHandshake(string str)
-    {
-        Debug.Log(str);
-        if (str == "GameIsland")
-            GotHandShake = true;
-    }
-
-    void CheckCallback(string str)
+    public void CheckCallback(string str)
     {
         switch (str)
         {
-            case "A":
+            case "A": 
                 Debug.Log("<color=blue>" + "A" + "</color>\n");
                 Camera.main.GetComponent<Camera>().backgroundColor = Color.blue;
             break;
-            case "B":
+            case "B": 
                 Debug.Log("<color=yellow>" + "B" + "</color>\n");
                 Camera.main.GetComponent<Camera>().backgroundColor = Color.yellow;
             break;
-            case "C":
+            case "C": 
                 Debug.Log("<color=red>" + "C" + "</color>\n");
                 Camera.main.GetComponent<Camera>().backgroundColor = Color.red;
             break;
-            case "H":
+            case "GameIsland": 
                 Debug.Log("<color=grey>" + "Cathed a spare handshake " + "</color>\n");
                 Camera.main.GetComponent<Camera>().backgroundColor = Color.magenta;
             break;
-            default:
+            default: 
                 Debug.Log("Button Up");
-                Camera.main.GetComponent<Camera>().backgroundColor = Color.gray;
+                Camera.main.GetComponent<Camera>().backgroundColor = Color.cyan;
             break;
         }
     }
